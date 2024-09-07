df545a20a9b4ec7257574419b474702f037c1a5d
Tauqir <tauqirbinmofid@gmail.com>
2024-09-07 23:57:42 +0600

    Initial Commit

---
+++ app/src/main/java/com/example/assignment001/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.assignment001;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private boolean isColorChanged = false;
+    private boolean isImageToggled = false;
+    private boolean isTextClicked = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        TextView textView = findViewById(R.id.myTextView);
+        ImageView imageView = findViewById(R.id.myImageView);
+        Button button = findViewById(R.id.myButton);
+        TextView toggleText = findViewById(R.id.toggleText);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button Clicked!", Toast.LENGTH_SHORT).show();
+
+
+                if (isColorChanged) {
+                    button.setBackgroundColor(Color.BLUE);
+                } else {
+                    button.setBackgroundColor(Color.RED);
+                }
+
+
+                isColorChanged = !isColorChanged;
+            }
+        });
+
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Image Clicked!", Toast.LENGTH_SHORT).show();
+
+                if (isImageToggled) {
+                    imageView.setImageResource(R.drawable.tauqir);
+                } else {
+                    imageView.setImageResource(R.drawable.tauqir1);
+                }
+                isImageToggled = !isImageToggled;
+            }
+        });
+
+
+        toggleText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Text Clicked!", Toast.LENGTH_SHORT).show();
+                if (isTextClicked) {
+                    toggleText.setText("IT IS A Clickable TEXT");
+                    toggleText.setTextColor(Color.BLUE);
+                } else {
+                    toggleText.setText("Text is clicked");
+                    toggleText.setTextColor(Color.RED);
+                }
+                isTextClicked = !isTextClicked;
+            }
+        });
+    }
+}
